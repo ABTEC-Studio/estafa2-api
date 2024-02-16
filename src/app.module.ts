b1d35f5ff010d9aad2b5d41d5e84109d55a9ed3e
Angelo Grangetto <angelograngetto@gmail.com>
2024-02-15 22:21:40 -0300

    add rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReportController } from './report/report.controller';
 import { ReportModule } from './report/report.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [ReportModule],
+  imports: [
+    ReportModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+  ],
   controllers: [AppController, ReportController],
   providers: [AppService],
 })
